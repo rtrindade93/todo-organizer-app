bcd8da14f056457c37bc38d8029a7e2fd22981b1
Ricardo Trindade <ricardopovoas.trindade@gmail.com>
2025-03-04 22:32:27 +0000

    initial commit. vue web app. nav component and todo view

---
+++ frontend-web/src/components/NavComponent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { RouterLink, useRoute } from 'vue-router'
+
+const isActive = (path: string): boolean => useRoute().path === path
+</script>
+
+<template>
+  <nav class="bg-gray-300 border-b">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex h-20 items-center justify-between">
+        <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+          <RouterLink
+            to="/"
+            :class="[
+              isActive('/') ? 'bg-gray-500' : '',
+              'text-black',
+              'hover:bg-gray-700',
+              'hover:text-white',
+              'px-3',
+              'py-2',
+              'rounded-md',
+            ]"
+            >To Do</RouterLink
+          >
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ frontend-web/src/router/index.ts
@@ -0,0 +1,15 @@
+import ToDosView from '@/views/ToDosView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'todos',
+      component: ToDosView,
+    },
+  ],
+})
+
+export default router
